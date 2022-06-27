5f80ce5dbb9f89c5c47c13ae074476d0cb097eea
Piyush Bagad <piyushnbagad11@gmail.com>
2022-06-27 08:30:04 +0200

    Adds GDT configs

--- run_multiple_experiments.sh
+++ run_multiple_experiments.sh
@@ -1,6 +1,6 @@
-cfg_1=configs/Charades/VSSL/32x8_112x112_R18_supervised_seed_1.yaml
-cfg_2=configs/Charades/VSSL/32x8_112x112_R18_supervised_seed_2.yaml
-cfg_3=configs/Charades/VSSL/32x8_112x112_R18_supervised_seed_3.yaml
+cfg_1=configs/Charades/VSSL/32x8_112x112_R18_gdt_seed_1.yaml
+cfg_2=configs/Charades/VSSL/32x8_112x112_R18_gdt_seed_2.yaml
+cfg_3=configs/Charades/VSSL/32x8_112x112_R18_gdt_seed_3.yaml
 
 CUDA_VISIBLE_DEVICES=0 bash scripts/jobs/train_on_charades.sh -c $cfg_1 -b 16 -n 1 &
 CUDA_VISIBLE_DEVICES=1 bash scripts/jobs/train_on_charades.sh -c $cfg_2 -b 16 -n 1 &
